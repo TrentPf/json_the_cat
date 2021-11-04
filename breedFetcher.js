e1d70241b1f6f1e27fd269c4d04607f7322290c1
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-04 13:39:48 -0400

    Refactor breedFetcher.js and appropriate test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
-const args = process.argv;
-const cat = args.slice(2);
-
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${cat[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (body === "[]") {
-    console.log(`Could not find search results for your query: ${cat[0]}`);
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("breed not found", null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
